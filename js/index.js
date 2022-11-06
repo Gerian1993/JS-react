d24dc0880b1586f7216f9351783d280f869a6edc
Ivan <i.generalov.93@mail.com>
2022-11-07 01:18:16 +0500

    feat: add cycles

--- js/index.js
+++ js/index.js
@@ -1,30 +1,59 @@
-'use strict';
+/*1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-const numberOfFilms = Number(prompt('Сколько фильмов вы уже посмотрели?', 'Введите число'));
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+"use strict";
+
+const numberOfFilms = Number(
+  prompt("Сколько фильмов вы уже посмотрели?", "Введите число")
+);
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
-let q1 = prompt('Один из последних просмотренных фильмов?', 'Название фильма');
-let q2 = prompt('На сколько оцените его?', 'Введите балл от 1 до 10');
+for (let i = 0; i < 2; i++) {
+  let q1 = prompt("Один из последних просмотренных фильмов?", "");
+  while (q1 === "" || q1 === null || q1.length > 50) {
+    q1 = prompt(
+      "Неккоретктно. Пожалуйста повторите какой один из последних просмотренных фильмов?",
+      ""
+    );
+  }
+  let q2 = Number(
+    prompt("На сколько оцените его?", "Введите число от 1 до 10")
+  );
+  while (q2 === "" || q2 === null || q2 > 10 || q2 < 1 || isNaN(q2)) {
+    q2 = Number(
+      prompt(
+        "Неккоретктно. Пожалуйста повторите на сколько оцените его?",
+        "Введите число от 1 до 10"
+      )
+    );
+  }
+  personalMovieDB.movies[q1] = q2;
+}
 
-let nameOfFilm = q1,
-markOfFilm = q2;
-
-personalMovieDB.movies[nameOfFilm] = markOfFilm;
-
-let q3 = prompt('Один из последних просмотренных фильмов?', 'Название фильма');
-let q4 = prompt('На сколько оцените его?', 'Введите балл от 1 до 10');
-
-nameOfFilm = q3;
-markOfFilm = q4;
-
-personalMovieDB.movies[nameOfFilm] = markOfFilm;
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
