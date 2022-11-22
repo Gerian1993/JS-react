c6db1ae27eece5584e9843683b9253518a4ea405
Ivan <i.generalov.93@mail.com>
2022-11-22 17:42:40 +0500

    feat: add some functionality

--- js/script.js
+++ js/script.js
@@ -1,50 +1,91 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 "use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const advs = document.querySelectorAll(".promo__adv img");
+  const bg = document.querySelector(".promo__bg");
+  const genre = bg.querySelector(".promo__genre");
+  const filmList = document.querySelector(".promo__interactive-list");
 
-const movieDB = {
-  movies: [
-    "Логан",
-    "Лига справедливости",
-    "Ла-ла лэнд",
-    "Одержимость",
-    "Скотт Пилигрим против...",
-  ],
-};
-let { movies } = movieDB;
-movies.sort();
+  const addForm = document.querySelector("form.add");
+  const addFilm = addForm.querySelector("input.adding__input");
+  const checkbox = addForm.querySelector('[type="checkbox"]');
+  const btnForm = addForm.querySelector("button");
+  
+  const movieDB = {
+    movies: [
+      "Логан",
+      "Лига справедливости",
+      "Ла-ла лэнд",
+      "Одержимость",
+      "Скотт Пилигрим против...",
+    ],
+  };
+  let { movies } = movieDB;
 
-const adv = document.querySelectorAll(".promo__adv img");
-const bg = document.querySelector(".promo__bg");
-const genre = bg.querySelector(".promo__genre");
-//const filmList = document.querySelectorAll(".promo__interactive-item");
-const films = document.querySelector(".promo__interactive-list");
+  genre.textContent = "драма";
+  bg.style.backgroundImage = 'url("../img/bg.jpg")';
 
-adv.forEach((item) => {
-  item.remove();
-});
+  const removeAdv = (adv) => {
+    adv.forEach((item) => {
+      item.remove(item);
+    });
+  };
 
-genre.textContent = "драма";
+  const createMovieDb = (arr, list) => {
+    arr.sort();
+    list.innerHTML = "";
 
-bg.style.backgroundImage = 'url("../img/bg.jpg")';
+    arr.forEach((film, i) => {
+      list.innerHTML += `
+      <li class="promo__interactive-item">${i + 1}. ${film}
+          <div class="delete"></div>
+      </li>`;
+    });
 
-films.innerHTML = "";
+    document.querySelectorAll(".delete").forEach((btn) => {
+      btn.addEventListener("click", () => {
+        console.log('vla');
+      });
+    });
+  };
 
-movies.forEach((film, i) => {
-  films.innerHTML += `
-    <li class="promo__interactive-item">${i + 1}. ${film}
-        <div class="delete"></div>
-    </li>`;
+  btnForm.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let inputFilm = addFilm.value;
+    let favorite = checkbox.checked;
+
+    if (inputFilm) {
+      if (inputFilm.length > 21) {
+        inputFilm = addFilm.value.slice(0, 21) + "...";
+        movies.push(inputFilm.toUpperCase());
+        createMovieDb(movies, filmList);
+      } else {
+        movies.push(inputFilm.toUpperCase());
+        createMovieDb(movies, filmList);
+      }
+    }
+    if (Boolean(favorite) === true && Boolean(inputFilm) === true) {
+      console.log("Добавляем любимый фильм");
+    }
+  });
+
+  removeAdv(advs);
+  createMovieDb(movies, filmList);
 });
